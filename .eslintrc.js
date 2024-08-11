a594ae826e37434d6c2fd78dff3d8df1f5452571
uniquexiaoming <uniquexiaoming@163.com>
2024-08-11 15:25:53 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['tuya-panel'],
+  rules: {
+    camelcase: 0,
+    'react/require-default-props': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
+};
